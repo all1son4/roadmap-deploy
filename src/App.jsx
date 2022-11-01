417d2676450ccf2714934955d5f950829ada2c3d
Vlad Shevchenko <vlad.shevchenko@kitrum.com>
2022-11-01 14:52:10 +0300

    fix: add name to h1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Here is successful deployed project</h1>
+          <h1>Here is successful deployed project by Dmitry Strok</h1>
         </header>
       </div>
   );
